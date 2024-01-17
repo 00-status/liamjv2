80d6e67d248fe1189ab0453d2fbc3fd6df2bf0cd
liam johnson <00-status@liams-MacBook-Pro.local>
2024-01-17 13:34:30 -0600

    set up static web app dev server

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
 import { ReactElement } from "react";
 
-import { Page } from "./SharedComponents/Page/Page";
 import { AboutMe } from "./AboutMe/AboutMe";
 
 export const App = (): ReactElement => {
-    return <Page title="Liam Johnson"><AboutMe /></Page>;
+    return <AboutMe />;
 };
