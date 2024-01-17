a4cf3b9ef160e32c107bf4a191eaa38d18216a1a
liam johnson <00-status@liams-MacBook-Pro.local>
2024-01-17 14:34:17 -0600

    Testing gh auth

--- src/DiceRoller/DiceRoller.tsx
+++ src/DiceRoller/DiceRoller.tsx
@@ -2,5 +2,5 @@ import { ReactElement } from "react";
 import { Page } from "../SharedComponents/Page/Page";
 
 export const DiceRoller = (): ReactElement => {
-    return <Page title="Dice Roller">Dice roller stuff</Page>;
+    return <Page title="Dice Roller">Dice roller stuff!</Page>;
 };
