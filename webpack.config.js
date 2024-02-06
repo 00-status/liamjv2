c2c896c41da33fca2d822218749a66dcc53669c8
00-status <dedypips@gmail.com>
2024-02-06 14:08:15 -0600

    removed unnecessary webpack rule

--- webpack.config.js
+++ webpack.config.js
@@ -16,10 +16,6 @@ module.exports = {
         generator: { filename: "fonts/[hash][ext][query]" }
       },
       {
-        test: /\.svg/,
-        type: 'asset/inline'
-      },
-      {
         test: /\.(js|jsx|ts|tsx)$/,
         exclude: [/(node_modules)/],
         loader: "babel-loader",
