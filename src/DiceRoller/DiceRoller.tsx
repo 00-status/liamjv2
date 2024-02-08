15db08c9c0f5b6a9bafe4387b47651d82f2df451
00-status <dedypips@gmail.com>
2024-02-08 17:22:17 -0600

    updated header styling. updated dice roller result spacing

--- src/DiceRoller/DiceRoller.tsx
+++ src/DiceRoller/DiceRoller.tsx
@@ -31,21 +31,21 @@ export const DiceRoller = (): ReactElement => {
             <div className="">
                 <h1>Dice Roller</h1>
                 <div className="dice-roller--summary">
-                    <div>
-                        <div className="dice_roller--result">
+                    <div className="dice-roller--results-cntainer">
+                        <div className="dice-roller--result">
                             <h1>{generatedNumber ? generatedNumber : "--"}</h1>
                             <hr className="divider" />
                             <div>Result</div>
                         </div>
-                        <div className="dice_roller--result">
+                        <div className="dice-roller--result">
                             <h1>{diceRolled ? diceRolled : "--"}</h1>
                             <hr className="divider" />
                             <div>Dice Rolled</div>
                         </div>
                     </div>
-                    <div className="dice_roller--log-container">
+                    <div className="dice-roller--log-container">
                         <h2>Log</h2>
-                        <textarea readOnly className="dice_roller--log" defaultValue={diceLog.join('\r\n')} />
+                        <textarea readOnly className="dice-roller--log" defaultValue={diceLog.join('\r\n')} />
                     </div>
                 </div>
             </div>
