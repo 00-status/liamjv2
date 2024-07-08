d933d5c1c8597884082d3dba0568077928e29f09
00-status <dedypips@gmail.com>
2024-07-07 18:27:16 -0600

    Added subtotal component

--- src/DnDShop/DnDShop.tsx
+++ src/DnDShop/DnDShop.tsx
@@ -7,6 +7,7 @@ import items from '../assets/items.json';
 import { Item } from "./types";
 import { Inventory } from "./Inventory";
 import { Cart } from "./Cart";
+import { SubTotal } from "./SubTotal";
 
 export type CartSlot = { droppableID: string, item: null | Item };
 
@@ -44,11 +45,12 @@ export const DndShop = () => {
         setCartSlots(cartSlotsCopy);
     };
 
-    return <Page title="The Shop">
+    return <Page title="Liam Johnson">
         <div className="dnd-shop">
-            <h1>The Cart</h1>
+            <h1>The Shop</h1>
             <DndContext onDragEnd={onDragEnd}>
                 <Cart cartSlots={cartSlots} />
+                <SubTotal cartItems={cartSlots} />
                 <Inventory items={items} />
             </DndContext>
         </div>
@@ -72,20 +74,10 @@ const generateInitialCartSlots = (): CartSlot[] => {
     //          Create a local state object that will track the amount of Platinum, Gold,
     //          silver, and copper coins that the player has.
     //      Create a PlayerCurrency Component that will receive the Currency object as props and display the money.    
-    // Listing Items:
-    //      Create a ShopInventory Component
-    //      Create a ShopIntentoryItem component - this will be the list item 
-    //      Put the ShopInventory component on the right-hand side of the page.
-    // Shopping Cart
-    //      Create a ShoppingCart Component that will take in an array of ShoppingCartItems
-    //      ShoppingCartItems have name, currency, cost, weight, row, and column properties.
-    //      The ShoppingCart will display the ShoppingCartItems based on their row and column.
-    //      The DnDShop Component will be in charge of managing the state of the ShoppingCartItems array.
-    //          One just has to pass down the setShoppingCartItems function to whatever child needs it.
     // Tracking Total Cost and Weight
     //      Create a new TotalCost component that takes in the list of ShoppingCartItems
     //          Total up the cost of all of the items
     //          Display the least amount of coins needed to make the transaction.
     //          That is, if we have 12 total copper, then we should convert that to 1 silver and 2 copper
-    // Tracking the Player's leftover Change
+    // Tracking the Player's leftover balance
     //      Create A PlayerChangeComponent
---
+++ src/DnDShop/SubTotal.tsx
@@ -0,0 +1,65 @@
+import { CartSlot } from "./DnDShop";
+import { Item } from "./types";
+
+type Props = {
+    cartItems: CartSlot[];
+};
+
+type Currency = {
+    gold: number;
+    silver: number;
+    copper: number;
+};
+
+export const SubTotal = (props: Props) => {
+    const { cartItems } = props;
+
+    // Reduce each item, with a initial value of { gold: 0, silver: 0, copper: 0 }
+    //      Add the item's cost to the appropriate "bucket"
+    // convert all possible copper into silver, and the silver into gold.
+    // i.e., one should have a result like this: { gold: 12, silver: 7, copper: 4 }
+
+    const filteredCartItems: Item[] = cartItems
+        .map((item) => item.item)
+        .filter((item) => {
+            return item !== null;
+        }
+    );
+
+    const itemTotalsByCurrency: Currency = filteredCartItems.reduce((carry, item) => {
+        switch (item.currency) {
+            case 'gold':
+                carry.gold += item.cost;
+                break;
+            case 'silver':
+                carry.silver += item.cost;
+                break;
+            case 'copper':
+                carry.copper += item.cost;
+                break;
+            default:
+                break;
+
+        }
+
+        return carry;
+    }, { gold: 0, silver: 0, copper: 0 });
+
+    const normalizedTotalsByCurrency = normalizeTotals(itemTotalsByCurrency);
+
+    return <div>
+        <h2>Subtotal</h2>
+        Gold: {normalizedTotalsByCurrency.gold} | Silver: {normalizedTotalsByCurrency.silver} | Copper: {normalizedTotalsByCurrency.copper}
+    </div>;
+};
+
+const normalizeTotals = (itemTotalsByCurrency: Currency): Currency => {
+    const remainingCopper = itemTotalsByCurrency.copper % 10;
+    const newSilver = Math.trunc(itemTotalsByCurrency.copper / 10);
+
+    const silver = newSilver + itemTotalsByCurrency.silver;
+    const remainingSilver = silver % 10;
+    const newGold = Math.trunc(silver / 10);
+
+    return { gold: itemTotalsByCurrency.gold + newGold, silver: remainingSilver, copper: remainingCopper };
+};
