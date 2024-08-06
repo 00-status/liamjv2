0ac4e8eb745f125fbb72428301f9c3881d9df8ae
00-status <dedypips@gmail.com>
2024-08-06 16:56:23 -0600

    Stubbed out a plan for adding individual projects. Audited for improvements.

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,25 @@ import { DiceRoller } from './DiceRoller/DiceRoller';
 import { WeaponMaker } from './WeaponMaker/WeaponMaker';
 import { DndShop } from './DnDShop/DnDShop';
 
+// TODO: Dice roller
+//      Replace "Result" and "Dice rolled" cards with new Card component
+//      Reduce dice button size on desktop
+// TODO: Weapon <aker
+//      Replace card with Card component
+//      Add an icon for the Generate weapon button
+//      Update Generate weapon button to use Button Component
+//      Update button colour to be blue.
+//      Add a label to the dropdown.
+//      Replace Dropdown library with Dropdown Component
+//      Update "Properties" and "Damage" inline titles to be bold.
+// TODO: Navigation
+//      Create routes for each project.
+//          That is, /rpg_tools/dialogue_tree or /dnd_tools/dice_roller
+//      Convert the main navigation to route to each project.
+//          So, the main nav would be "RPG Tools" and "D&D Tools"
+//      Add a back button to each project's navigation.
+//      Create a 404 page.
+
 const rootDomNode = document.getElementById('app');
 
 if (!rootDomNode) {
