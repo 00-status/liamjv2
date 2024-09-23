8a5591dcc7bfdf6891fe11603983ea7d2b148919
00-status <dedypips@gmail.com>
2024-09-23 15:50:37 -0600

    calling api

--- src/DNDTools/WeaponMaker/WeaponMakerV2.tsx
+++ src/DNDTools/WeaponMaker/WeaponMakerV2.tsx
@@ -3,19 +3,22 @@ import { Card } from "../../SharedComponents/Card/Card";
 import { GearsIcon } from "../../SharedComponents/Icons/GearsIcon";
 import { Page } from "../../SharedComponents/Page/Page";
 import { dndRoutes } from "../domain";
+import { useGenerateWeapon } from "./useGenerateWeapon";
 
 export const WeaponMakerV2 = () => {
+    const {weapon, generateWeapon} = useGenerateWeapon();
+
     return <Page routes={dndRoutes} title="D&D Tools">
         <div>
             <h1>Weapon Maker</h1>
         </div>
         <div>
-            <Card title="Weapon">
+            {weapon && <Card title={weapon.defaultName}>
                 Weapon
-            </Card>
+            </Card>}
         </div>
         <div>
-            <Button>
+            <Button onClick={generateWeapon}>
                 <GearsIcon /> Generate weapon
             </Button>
         </div>
