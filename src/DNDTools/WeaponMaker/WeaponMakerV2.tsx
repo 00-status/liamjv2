2816a84ffdb60c7b4af1a37b3b227e8a1f963fbd
00-status <dedypips@gmail.com>
2024-09-25 14:29:27 -0600

    Can now submit weapon rarity in request

--- src/DNDTools/WeaponMaker/WeaponMakerV2.tsx
+++ src/DNDTools/WeaponMaker/WeaponMakerV2.tsx
@@ -1,20 +1,22 @@
 
 import './weapon-maker.css';
-import { Button, ButtonTheme } from "../../SharedComponents/Button/Button";
+import { Button } from "../../SharedComponents/Button/Button";
 import { GearsIcon } from "../../SharedComponents/Icons/GearsIcon";
 import { Page } from "../../SharedComponents/Page/Page";
 import { dndRoutes } from "../domain";
 import { useGenerateWeapon } from "./useGenerateWeapon";
 import { WeaponCard } from './WeaponCard';
+import { Dropdown } from '../../SharedComponents/Dropdown/Dropdown';
+import { useState } from 'react';
 
 // ToDo:
 //      Generate Form
 //          Add filters above the generate weapon button.
-//          After the weapon loads, the "Generate Weapon" button should slide up to make room for the weapon card.
-//          Add rising sparkles when a user hovers their cursor over the button.
-//          Consider adding a fancy, filigre border to the generate weapon button. 
 //          Add a dropdown, so the user can pick the rarity level of their weapon.
 //          Add a dropdown, so the user can pick the weapon type of their weapon.
+//          After the weapon loads, the "Generate Weapon" button should slide up to make room for the weapon card.
+//          Add rising sparkles when a user hovers their cursor over the button. ✅
+//          Consider adding a fancy, filigre border to the generate weapon button. 
 //          Disable the "generate weapon" button while the API request is happening (and perhaps for some time after).
 //      Weapon Card
 //          Add more interesting colours to the Weapon Card component.
@@ -23,19 +25,41 @@ import { WeaponCard } from './WeaponCard';
 //      General
 //          Make a more friendly name for the weapon generator.
 //          Add AI disclaimer to the footer.
+//          Fix API error: Candidate was missing "content" property.
+//          When a weapon's base damage is the same as its extra damage, add the number of dice together instead of displaying both.
 
 export const WeaponMakerV2 = () => {
+    const [selectedRarity, setSelectedRarity] = useState<string>("Uncommon");
+
     const {weapon, generateWeapon} = useGenerateWeapon();
+
+    const rarities = [
+        {value: "Uncommon", label: "Uncommon"},
+        {value: "Rare", label: "Rare"},
+        {value: "Very Rare", label: "Very Rare"},
+        {value: "Legendary", label: "Legendary"},
+    ];
 
     return <Page routes={dndRoutes} title="D&D Tools">
         <div className='weapon-maker'>
             <div>
                 <h1>Weapon Maker</h1>
             </div>
-            <div className="weapon-maker__button">
-                <Button hasSheen onClick={generateWeapon}>
-                    <GearsIcon /> Generate weapon
-                </Button>
+            <div className='weapon-maker__filters'>
+                <div className='weapon-maker__dropdown'>
+                    <Dropdown defaultValue="Uncommon" options={rarities}
+                        onOptionSelect={(option) => {
+                            if (option) {
+                                setSelectedRarity(option);
+                            }
+                        }}
+                    />
+                </div>
+                <div className="weapon-maker__button">
+                    <Button hasSheen onClick={() => generateWeapon(selectedRarity)}>
+                        <GearsIcon /> Generate weapon
+                    </Button>
+                </div>
             </div>
             <div>
                 {weapon && <WeaponCard weapon={weapon} />}
--- src/DNDTools/WeaponMaker/useGenerateWeapon.tsx
+++ src/DNDTools/WeaponMaker/useGenerateWeapon.tsx
@@ -3,14 +3,14 @@ import { Weapon } from "./domain/types";
 
 type UseGenerateWeapon = {
     weapon: Weapon | null;
-    generateWeapon: () => void;
+    generateWeapon: (rarity: string) => void;
 };
 
 export const useGenerateWeapon = (): UseGenerateWeapon => {
     const [weapon, setWeapon] = useState<Weapon|null>(null);
 
-    const generateWeapon = useCallback(() => {
-        fetch("/api/1/generate_weapon")
+    const generateWeapon = useCallback((rarity: string) => {
+        fetch("/api/1/generate_weapon?rarity=" + rarity)
             .then((response: Response) => response.json())
             .then(json => {
                 console.log(json);
