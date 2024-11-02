c555dec222d1310901e4e1b3a8fadd168b58c8bd
00-status <dedypips@gmail.com>
2024-11-02 17:34:29 -0600

    Can navigate to various weapon effects via the list page.

--- src/HiddenPages/WeaponEffectList/WeaponEffectList.tsx
+++ src/HiddenPages/WeaponEffectList/WeaponEffectList.tsx
@@ -2,16 +2,26 @@ import { useNavigate } from "react-router-dom";
 
 import { Page } from "../../SharedComponents/Page/Page";
 import { useFetchWeaponEffect } from "./useFetchWeaponEffect";
+import { WeaponEffect } from "../WeaponEffectEditor/WeaponEffectForm";
 
 export const WeaponEffectList = () => {
     const weaponEffects = useFetchWeaponEffect();
     const navigate = useNavigate();
 
+    const navigateToWeaponEffectForm = (weaponEffect: WeaponEffect) => {
+        navigate("/unlisted/weapon_effect?id=" + (weaponEffect.id ?? 0))
+    };
+
     return <Page title="Weapon Effects" routes={[{label: "Landing", route: "/", isHomeLink: true}]}>
         <div>
             <h1>Weapon Effect List</h1>
             <div>
-                {weaponEffects.map((weaponEffect) => <div key={weaponEffect.name} >{weaponEffect.name}</div>)}
+                {weaponEffects.map((weaponEffect) => <div
+                    onClick={() => navigateToWeaponEffectForm(weaponEffect)} key={weaponEffect.name}
+                    >
+                        {weaponEffect.name}
+                    </div>
+                )}
             </div>
         </div>
     </Page>
