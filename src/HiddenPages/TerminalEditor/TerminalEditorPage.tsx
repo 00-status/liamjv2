868657e14448c439f0baf9418dda62f514a08a01
00-status <dedypips@gmail.com>
2024-12-26 11:20:03 -0600

    Can now create servers

--- src/HiddenPages/TerminalEditor/TerminalEditorPage.tsx
+++ src/HiddenPages/TerminalEditor/TerminalEditorPage.tsx
@@ -1,5 +1,9 @@
+import { useEffect, useState } from "react";
 import { Page } from "../../SharedComponents/Page/Page";
+import { TextInput } from "../../SharedComponents/TextInput/TextInput";
 import { useServers } from "./hooks/useServers";
+import { Button } from "../../SharedComponents/Button/Button";
+import { useCreateServer } from "./hooks/useCreateServer";
 
 export const TerminalEditorPage = () => {
     // ToDO
@@ -8,11 +12,22 @@ export const TerminalEditorPage = () => {
     //      Fetch Directories
     //      Create Directories
 
-    const { servers } = useServers();
+    const [newServerName, setNewServerName] = useState<string>("");
+
+    const { servers, fetchServers } = useServers();
+    const { createServer } = useCreateServer();
+
+    useEffect(() => {
+        fetchServers();
+    }, []);
 
     return <Page title="Terminal Editor" routes={[]}>
         <div>
             <div>
+                <div>
+                    <TextInput value={newServerName} onChange={(value) => setNewServerName(value || "")} />
+                    <Button onClick={() => createServer(newServerName)} >Submit</Button>
+                </div>
                 {servers.map(server => server.name)}
             </div>
             <div>
