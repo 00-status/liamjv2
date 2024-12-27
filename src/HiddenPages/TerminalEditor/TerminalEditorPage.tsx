9c48778c296764b1e9b523bc6d2d623344f8dfe4
00-status <dedypips@gmail.com>
2024-12-27 10:36:49 -0600

    Can now create new directories.

--- src/HiddenPages/TerminalEditor/TerminalEditorPage.tsx
+++ src/HiddenPages/TerminalEditor/TerminalEditorPage.tsx
@@ -6,7 +6,7 @@ import { TextInput } from "../../SharedComponents/TextInput/TextInput";
 import { Button } from "../../SharedComponents/Button/Button";
 import { ServerItem } from "./ServerItem";
 import { useServers } from "./hooks/server/useServers";
-import { useDirectories } from "./hooks/directories/useDirectories";
+import { Directory, useDirectories } from "./hooks/directories/useDirectories";
 
 export const TerminalEditorPage = () => {
     // ToDO
@@ -22,8 +22,10 @@ export const TerminalEditorPage = () => {
     const [newServerName, setNewServerName] = useState<string>("");
     const [selectedServerId, setSelectedServerId] = useState<number|null>(null);
 
+    const [newDirectoryName, setNewDirectoryName] = useState<string>("");
+
     const { servers, fetchServers, createServer, deleteServer } = useServers();
-    const { directories, fetchDirectories } = useDirectories();
+    const { directories, fetchDirectories, createDirectory } = useDirectories();
 
     useEffect(() => {
         fetchServers();
@@ -36,7 +38,10 @@ export const TerminalEditorPage = () => {
     }, [selectedServerId]);
 
     const onServerDelete = (serverId: number) => {
-        setSelectedServerId(null);
+        if (serverId === selectedServerId) {
+            setSelectedServerId(null);
+        }
+
         deleteServer(serverId);
     };
 
@@ -44,7 +49,11 @@ export const TerminalEditorPage = () => {
         <div className="terminal-editor-page">
             <div className="terminal-editor-page__server-list">
                 <div className="terminal-editor-page__server-input">
-                    <TextInput placeholder="server name" value={newServerName} onChange={(value) => setNewServerName(value || "")} />
+                    <TextInput
+                        placeholder="server name"
+                        value={newServerName}
+                        onChange={(value) => setNewServerName(value || "")}
+                    />
                     <Button onClick={() => createServer(newServerName)} >Submit</Button>
                 </div>
                 {servers.map(server => <ServerItem
@@ -55,10 +64,29 @@ export const TerminalEditorPage = () => {
                 />)}
             </div>
             <div>
-                <div className="terminal-editor-page__server-input">
-                    <TextInput placeholder="server name" value={newServerName} onChange={(value) => setNewServerName(value || "")} />
-                    <Button onClick={() => createServer(newServerName)}>Submit</Button>
-                </div>
+                {selectedServerId && <div className="terminal-editor-page__server-input">
+                    <TextInput
+                        placeholder="directory name"
+                        value={newDirectoryName}
+                        onChange={(value) => setNewDirectoryName(value || "")}
+                    />
+                    <Button onClick={() => {
+                        if (selectedServerId && newDirectoryName) {
+                            const newDirectory: Directory = {
+                                id: 0,
+                                serverId: selectedServerId,
+                                name: newDirectoryName,
+                                dateCreated: (new Date()).toISOString(),
+                                parentDirectory: 0,
+                                subDirectories: [],
+                                files: []
+                            };
+                            createDirectory(newDirectory);
+                        }
+                    }}>
+                        Submit
+                    </Button>
+                </div>}
                 {directories.map(directory => directory.name)}
             </div>
             <div>
--- src/HiddenPages/TerminalEditor/hooks/directories/useCreateDirectory.ts
+++ src/HiddenPages/TerminalEditor/hooks/directories/useCreateDirectory.ts
@@ -8,18 +8,24 @@ type UseCreateDirectory = {
 
 export const useCreateDirectory = (fetchDirectories: (serverId: number) => void): UseCreateDirectory => {
     const createDirectory = useCallback((directory: Directory) => {
-        const directoryJson = JSON.stringify(directory);
+        const newDirectory = {
+            id: directory.id,
+            server_id: directory.serverId,
+            name: directory.name,
+            date_created: directory.dateCreated,
+            parent_directory: directory.parentDirectory,
+            sub_directories: directory.subDirectories,
+            files: directory.files,
+        };
+
+        const directoryJson = JSON.stringify(newDirectory);
 
         fetch("/api/1/terminal_directories", {
             method: "POST",
             body: directoryJson,
             headers: { "Content-type": "application/json; charset=UTF-8" }
         })
-            .then((response) => response.json())
-            .then((json) => {
-                console.log(json);
-                fetchDirectories(directory.serverId);
-        });
+            .then((response) => fetchDirectories(directory.serverId));
     }, []);
 
     return {
