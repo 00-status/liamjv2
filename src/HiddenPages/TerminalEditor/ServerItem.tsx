b74718e2a32e6998060dc4c2283a16783fdf47a4
00-status <dedypips@gmail.com>
2024-12-27 09:46:55 -0600

    Clicking a server item now fetches the associated directories

--- src/HiddenPages/TerminalEditor/ServerItem.tsx
+++ src/HiddenPages/TerminalEditor/ServerItem.tsx
@@ -7,13 +7,14 @@ import { Server } from "./hooks/server/useServers";
 type Props = {
     server: Server;
     onServerDelete: (serverId: number) => void;
+    onServerClick: () => void;
 };
 
 export const ServerItem = (props: Props) => {
-    const { server, onServerDelete } = props;
+    const { server, onServerDelete, onServerClick } = props;
 
     return <div className="server-item">
-        <div className="server-item__text">
+        <div className="server-item__text" onClick={onServerClick}>
             {server.name}
         </div>
         <Button buttonTheme={ButtonTheme.Delete} onClick={() => onServerDelete(server.id)}>
--- src/HiddenPages/TerminalEditor/TerminalEditorPage.tsx
+++ src/HiddenPages/TerminalEditor/TerminalEditorPage.tsx
@@ -6,6 +6,7 @@ import { TextInput } from "../../SharedComponents/TextInput/TextInput";
 import { Button } from "../../SharedComponents/Button/Button";
 import { ServerItem } from "./ServerItem";
 import { useServers } from "./hooks/server/useServers";
+import { useDirectories } from "./hooks/directories/useDirectories";
 
 export const TerminalEditorPage = () => {
     // ToDO
@@ -13,30 +14,52 @@ export const TerminalEditorPage = () => {
     //      Create Server ✅
     //      Delete Server ✅
     //      Directories
-    //          READ
+    //          READ ✅
     //          Create
     //          Delete
     //      Create Directories
 
     const [newServerName, setNewServerName] = useState<string>("");
+    const [selectedServerId, setSelectedServerId] = useState<number|null>(null);
 
     const { servers, fetchServers, createServer, deleteServer } = useServers();
+    const { directories, fetchDirectories } = useDirectories();
 
     useEffect(() => {
         fetchServers();
     }, []);
 
+    useEffect(() => {
+        if (selectedServerId) {
+            fetchDirectories(selectedServerId);
+        }
+    }, [selectedServerId]);
+
+    const onServerDelete = (serverId: number) => {
+        setSelectedServerId(null);
+        deleteServer(serverId);
+    };
+
     return <Page title="Terminal Editor" routes={[]}>
         <div className="terminal-editor-page">
             <div className="terminal-editor-page__server-list">
                 <div className="terminal-editor-page__server-input">
-                    <TextInput value={newServerName} onChange={(value) => setNewServerName(value || "")} />
+                    <TextInput placeholder="server name" value={newServerName} onChange={(value) => setNewServerName(value || "")} />
                     <Button onClick={() => createServer(newServerName)} >Submit</Button>
                 </div>
-                {servers.map(server => <ServerItem key={server.id} server={server} onServerDelete={deleteServer} />)}
+                {servers.map(server => <ServerItem
+                    key={server.id}
+                    server={server}
+                    onServerDelete={onServerDelete}
+                    onServerClick={() => setSelectedServerId(server.id)}
+                />)}
             </div>
             <div>
-                Directory List
+                <div className="terminal-editor-page__server-input">
+                    <TextInput placeholder="server name" value={newServerName} onChange={(value) => setNewServerName(value || "")} />
+                    <Button onClick={() => createServer(newServerName)}>Submit</Button>
+                </div>
+                {directories.map(directory => directory.name)}
             </div>
             <div>
                 Directory Editor
