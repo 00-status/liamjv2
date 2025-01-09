9426ab4e4ef921769331b7a347ec5f5a7b35c68d
00-status <dedypips@gmail.com>
2025-01-09 15:13:55 -0600

    removed data file

--- src/Terminal/domain/types.ts
+++ src/Terminal/domain/types.ts
@@ -8,22 +8,6 @@ import { ListHandler } from "./Commands/ListHandler";
 import { OpenHandler } from "./Commands/OpenHandler";
 import { WorkingDirectoryHandler } from "./Commands/WorkingDirectoryHandler";
 
-export type TerminalDirectory = {
-    name: string;
-    dateCreated: string; // 2024-01-01 00:00:00
-    parent: string | null;
-    subDirectories: Array<string>;
-    files: Map<string, TerminalFile>;
-}
-
-export type TerminalFile = {
-    name: string;
-    contents: string;
-    creatorUsername: string;
-    dateCreated: string;
-    dateModified: string;
-};
-
 export type Command = {
     id: string;
     text: string;
