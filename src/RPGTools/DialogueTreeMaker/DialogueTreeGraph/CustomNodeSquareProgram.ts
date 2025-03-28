3c80c79638319f500102b0a32572fab54cda4f57
00-status <dedypips@gmail.com>
2025-03-28 11:51:37 -0600

    hover state now follows node color scheme

--- src/RPGTools/DialogueTreeMaker/DialogueTreeGraph/CustomNodeSquareProgram.ts
+++ src/RPGTools/DialogueTreeMaker/DialogueTreeGraph/CustomNodeSquareProgram.ts
@@ -2,10 +2,11 @@ import { NodeSquareProgram } from "@sigma/node-square";
 import { PlainObject } from "sigma/types";
 
 const squareHoverRenderer = function (context: CanvasRenderingContext2D, data: PlainObject, settings: PlainObject) {
-    const { x, y, size: nodeSize } = data;
+    const { x, y, size: nodeSize, color } = data;
 
     const hoverSize = nodeSize * 1.1;
 
+    context.fillStyle = color;
     context.fillRect(x - hoverSize, y - hoverSize, hoverSize * 2, hoverSize * 2);
 };
 
--- src/RPGTools/DialogueTreeMaker/DialogueTreeGraph/DefaultDrawHover.ts
+++ src/RPGTools/DialogueTreeMaker/DialogueTreeGraph/DefaultDrawHover.ts
@@ -1,12 +1,12 @@
 import { PlainObject } from "sigma/types";
 
 export const defaultCircleDrawHover = function (context: CanvasRenderingContext2D, data: PlainObject, settings: PlainObject) {
-    const { x, y, size: nodeSize } = data;
+    const { x, y, size: nodeSize, color } = data;
 
     const hoverSize = nodeSize * 1.1;
 
     context.beginPath();
     context.arc(x, y, hoverSize, 0, Math.PI * 2);
-    context.fillStyle = "#d6a840";
+    context.fillStyle = color;
     context.fill();
 };
