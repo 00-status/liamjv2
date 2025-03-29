dfaed6ebaeeada0c25e0b1a7acd8364110d585a2
00-status <dedypips@gmail.com>
2025-03-29 16:14:15 -0600

    updated hidden info modal

--- src/RPGTools/DialogueTreeMaker/DialogueMaker/HiddenInfo/UpdateHiddenInfoModal.tsx
+++ src/RPGTools/DialogueTreeMaker/DialogueMaker/HiddenInfo/UpdateHiddenInfoModal.tsx
@@ -4,10 +4,12 @@ import './update-hidden-info-modal.css';
 import { HiddenInfo, HiddenInfoCondition } from "../../domain/types";
 import { HiddenInfoConditions } from "./HiddenInfoConditions";
 import { Modal } from "../../../../SharedComponents/Modal/Modal";
-import { Button } from "../../../../SharedComponents/Button/Button";
+import { Button, ButtonTheme } from "../../../../SharedComponents/Button/Button";
+import { TrashIcon } from "../../../../SharedComponents/Icons/TrashIcon";
+import { TextInput } from "../../../../SharedComponents/TextInput/TextInput";
 
 type Props = {
-    hiddenInfoToEdit?: HiddenInfo;
+    hiddenInfoToEdit: HiddenInfo | null;
     isOpen: boolean;
     onClose: () => void;
     onSave: (hiddenInfo: HiddenInfo) => void;
@@ -16,41 +18,80 @@ type Props = {
 export const UpdateHiddenInfoModal = (props: Props) => {
     const { hiddenInfoToEdit, isOpen, onClose, onSave} = props;
 
-    const [hiddenInfo, setHiddenInfo] = useState<HiddenInfo>(hiddenInfoToEdit
-        ? hiddenInfoToEdit
-        : { id: crypto.randomUUID(), conditionIDs: [], description: '' }
+    const [conditionIDs, setConditionIds] = useState<Array<HiddenInfoCondition>>(
+        hiddenInfoToEdit?.conditionIDs ?? []
     );
+    const [description, setDescription] = useState<string | null>(hiddenInfoToEdit?.description ?? null);
 
-    useEffect(() => {
-        setHiddenInfo(hiddenInfoToEdit
-            ? hiddenInfoToEdit
-            : { id: crypto.randomUUID(), conditionIDs: [], description: '' }
-        );
-    }, [isOpen]);
+    const [newConditionID, setNewConditionID] = useState<string | null>(null);
+    const [newConditionName, setNewConditionName] = useState<string | null>(null);
+
+    const addCondition = () => {
+        if (!newConditionID || !newConditionName) {
+            return;
+        }
+
+        setConditionIds([...conditionIDs, { id: newConditionID, name: newConditionName }]);
+    };
+
+    const saveHiddenInfo = () => {
+        if (conditionIDs.length <= 0 || !description) {
+            return;
+        }
+
+        const newHiddenInfo: HiddenInfo = {
+            id: hiddenInfoToEdit?.id ?? crypto.randomUUID(),
+            conditionIDs,
+            description
+        };
+
+        onSave(newHiddenInfo);
+        onClose();
+    };
 
     return <Modal
         title={'Add Hidden Info'}
         isOpen={isOpen}
         onClose={onClose}
-        footer={<Button onClick={() => onSave(hiddenInfo)}>Save hidden info</Button>}
+        footer={<Button onClick={saveHiddenInfo}>Save</Button>}
     >
         <div className="add-hidden-info-modal">
-            <HiddenInfoConditions
-                conditions={hiddenInfo.conditionIDs}
-                updateConditions={(newConditions: Array<HiddenInfoCondition>) => {
-                    const newHiddenInfo = { ...hiddenInfo, conditionIDs: newConditions };
-
-                    setHiddenInfo(newHiddenInfo);
+            <TextInput
+                placeholder="Condition ID"
+                value={newConditionID ?? ""}
+                onChange={(newValue) => {
+                    setNewConditionID(newValue ?? '');
                 }}
             />
+            <TextInput
+                placeholder="Condition name"
+                value={newConditionName ?? ""}
+                onChange={(newValue) => {
+                    setNewConditionName(newValue ?? '');
+                }}
+            />
+            <Button disabled={!(newConditionID && newConditionName)} onClick={addCondition}>
+                Add condition
+            </Button>
+            {conditionIDs.map((condition) => {
+                return <div className="hidden-info-item">
+                    <div className="hidden-info-item__segment">
+                        {condition.id} | {condition.name}
+                    </div>
+                    <div className="hidden-info-item__segment--actions">
+                        <Button buttonTheme={ButtonTheme.Delete}>
+                            <TrashIcon />
+                        </Button>
+                    </div>
+                </div>;
+            })}
             <textarea
                 className="add-hidden-info-modal__description"
-                value={hiddenInfo.description}
+                value={description ?? ""}
                 onChange={(event) => {
                     const newValue = event.target.value ?? '';
 
-                    const newHiddenInfo = { ...hiddenInfo, description: newValue };
-                    setHiddenInfo(newHiddenInfo);
+                    setDescription(newValue);
                 }}
             />
         </div>
