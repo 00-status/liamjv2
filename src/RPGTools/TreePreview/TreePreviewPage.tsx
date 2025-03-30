66fc4816a12e0d233f02d63da0025651008afea4
00-status <dedypips@gmail.com>
2025-03-30 09:44:08 -0600

    added preview page

---
+++ src/RPGTools/TreePreview/TreePreviewPage.tsx
@@ -0,0 +1,10 @@
+import { Page } from "../../SharedComponents/Page/Page";
+import { RPGRoutes } from "../domain";
+
+export const TreePreviewPage = () => {
+    return <Page routes={RPGRoutes} title="RPG Tools">
+        <div>
+            Preview!
+        </div>
+    </Page>;
+};
