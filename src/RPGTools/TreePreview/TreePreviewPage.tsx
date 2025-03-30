ad45214861e2e1ebc328d9ac34775e5401300f40
00-status <dedypips@gmail.com>
2025-03-30 10:12:13 -0600

    stubbed out an algorithm

--- src/RPGTools/TreePreview/TreePreviewPage.tsx
+++ src/RPGTools/TreePreview/TreePreviewPage.tsx
@@ -2,9 +2,37 @@ import { Page } from "../../SharedComponents/Page/Page";
 import { RPGRoutes } from "../domain";
 
 export const TreePreviewPage = () => {
+    // list of dialogues
+    // current conditions
+    // current choices
+
+    // On start
+    //      Find dialogue with ID 1
+    //      Add dialogue 1's description to dialogueHistory
+    //      Display choices
+
+    // When a choice is clicked,
+    //      Find next node via ID.
+    //      If node is dialogue
+    //          Add new dialogue's description to history.
+    //          load new choices.
+    //      if node is Skill Test
+    //          Add skill test details to history
+    //          display difficulties as though they were choices
+
     return <Page routes={RPGRoutes} title="RPG Tools">
         <div>
-            Preview!
+            <div>
+                Conditions
+            </div>
+            <div>
+                <div>
+                    Dialogue History
+                </div>
+                <div>
+                    Choice List
+                </div>
+            </div>
         </div>
     </Page>;
 };
