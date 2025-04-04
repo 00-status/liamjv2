4cc784fccf2dc600a67c899cd85f5c8010422723
00-status <dedypips@gmail.com>
2025-04-04 09:49:47 -0600

    using symbols instead of the word for adding/removing

--- src/RPGTools/DialogueTreeMaker/DialogueMaker/Choices/ChoicesTable.tsx
+++ src/RPGTools/DialogueTreeMaker/DialogueMaker/Choices/ChoicesTable.tsx
@@ -72,7 +72,7 @@ export const ChoicesTable = (props: Props) => {
                     <div>{choice.nextDialogueID}</div>
                     <div>
                         {choice.conditionOutcomes.map(outcome => <div key={outcome.id}>
-                            {outcome.addingOrRemoving} | {outcome.id}
+                            {(outcome.addingOrRemoving === "adding" ? "+ " : "- ") + outcome.id}
                         </div>)}
                     </div>
                     <div>
