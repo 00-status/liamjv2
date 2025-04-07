e0b482df92401df1f4bb2694b6667fad0374ddd4
00-status <dedypips@gmail.com>
2025-04-07 11:03:15 -0600

    fixed home icon theme bug

--- src/SharedComponents/Page/Page.tsx
+++ src/SharedComponents/Page/Page.tsx
@@ -4,7 +4,7 @@ import { gtag } from "ga-gtag";
 
 import "./app.css";
 import { ImageButton } from "../ImageButton/ImageButton";
-import { HomeIcon } from "../Icons/HomeIcon";
+import { HomeIcon, HomeThemes } from "../Icons/HomeIcon";
 import { ToastMessage, ToastMessageContext } from "../Toast/ToastMessageContext";
 import { Toast } from "../Toast/Toast";
 
@@ -69,7 +69,7 @@ export const Page = (props: Props): ReactElement => {
                         const classNames = 'nav-item' + (isCurrentRoute ? ' nav-item__current' : '');
 
                         return <a key={route.route} className={classNames} onClick={() => goToRoute(route.route)}>
-                            { route.isHomeLink && <HomeIcon /> }
+                            { route.isHomeLink && <HomeIcon theme={HomeThemes.DARK} /> }
                             {route.label}
                         </a>;
                     })}
