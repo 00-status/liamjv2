b99e1b540f7ab81dd65f5e1f7f00b3214e68619b
00-status <dedypips@gmail.com>
2025-04-09 10:02:29 -0600

    created some records to map an icon type to the JSX element

--- src/SharedComponents/Icon/Icon.tsx
+++ src/SharedComponents/Icon/Icon.tsx
@@ -1,11 +1,81 @@
 
+import { ReactNode } from 'react';
+import { CloseIcon } from '../Icons/CloseIcon';
 import './icon.css';
+import { CopyIcon } from '../Icons/CopyIcon';
+import { Dice4 } from '../Icons/Dice4';
+import { Dice6 } from '../Icons/Dice6';
+import { Dice8 } from '../Icons/Dice8';
+import { Dice10 } from '../Icons/Dice10';
+import { Dice12 } from '../Icons/Dice12';
+import { Dice20 } from '../Icons/Dice20';
+import { DownloadIcon } from '../Icons/DownloadIcon';
+import { DragIcon } from '../Icons/DragIcon';
+import { EditFileIcon } from '../Icons/EditFileIcon';
+import { GearsIcon } from '../Icons/GearsIcon';
+import { HomeIcon } from '../Icons/HomeIcon';
+import { PencilIcon } from '../Icons/PencilIcon';
+import { PlusIcon } from '../Icons/PlusIcon';
+import { TauntIcon } from '../Icons/TauntIcon';
+import { TrashIcon } from '../Icons/TrashIcon';
+import { UploadIcon } from '../Icons/UploadFile';
+import { VeryDissatisfiedIcon } from '../Icons/VeryDissatisfiedIcon';
 
 // size: {small, medium, large}
 // iconTheme: { dark, light, neon }
 // iconType: { close, copy, dice4, etc }
 
-export const Icon = () => {
+export enum IconType {
+    CLOSE,
+    COPY,
+    DICE4,
+    DICE6,
+    DICE8,
+    DICE10,
+    DICE12,
+    DICE20,
+    DOANLOAD,
+    DRAG,
+    EDIT_FILE,
+    GEARS,
+    HOME,
+    PENCIL,
+    PLUS,
+    TAUNT,
+    TRASH,
+    UPLOAD_FILE,
+    VERY_DISSATISFIED
+};
+
+const iconComponents: Record<IconType, ReactNode> = {
+    [IconType.CLOSE]: <CloseIcon />,
+    [IconType.COPY]: <CopyIcon />,
+    [IconType.DICE4]: <Dice4 />,
+    [IconType.DICE6]: <Dice6 />,
+    [IconType.DICE8]: <Dice8 />,
+    [IconType.DICE10]: <Dice10 />,
+    [IconType.DICE12]: <Dice12 />,
+    [IconType.DICE20]: <Dice20 />,
+    [IconType.DOANLOAD]: <DownloadIcon />,
+    [IconType.DRAG]: <DragIcon />,
+    [IconType.EDIT_FILE]: <EditFileIcon />,
+    [IconType.GEARS]: <GearsIcon />,
+    [IconType.HOME]: <HomeIcon />,
+    [IconType.PENCIL]: <PencilIcon />,
+    [IconType.PLUS]: <PlusIcon />,
+    [IconType.TAUNT]: <TauntIcon />,
+    [IconType.TRASH]: <TrashIcon />,
+    [IconType.UPLOAD_FILE]: <UploadIcon />,
+    [IconType.VERY_DISSATISFIED]: <VeryDissatisfiedIcon />,
+};
+
+type Props = {
+    iconType: IconType;
+};
+
+export const Icon = (props: Props) => {
+    const { iconType } = props;
+
     return <svg className="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960">
     </svg>;
 };
