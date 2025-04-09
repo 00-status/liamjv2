5900fdb71e7a202d32444ddfe58991ded746bef3
00-status <dedypips@gmail.com>
2025-04-09 09:41:13 -0600

    Added a base icon component

---
+++ src/SharedComponents/Icon/Icon.tsx
@@ -0,0 +1,11 @@
+
+import './icon.css';
+
+// size: {small, medium, large}
+// iconTheme: { dark, light, neon }
+// iconType: { close, copy, dice4, etc }
+
+export const Icon = () => {
+    return <svg className="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960">
+    </svg>;
+};
