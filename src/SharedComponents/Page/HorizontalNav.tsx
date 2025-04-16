f7b8a02f1fb9987521444f137a0111f9ff7fd7a2
00-status <dedypips@gmail.com>
2025-04-16 10:01:21 -0600

    styling mobile navbar

--- src/SharedComponents/Page/HorizontalNav.tsx
+++ src/SharedComponents/Page/HorizontalNav.tsx
@@ -2,6 +2,8 @@ import { useLocation, useNavigate } from "react-router-dom";
 
 import './horizontal-nav.css';
 import { PageLink } from "./domain";
+import { Icon } from "../Icon/Icon";
+import { IconTheme, IconType } from "../Icon/domain";
 
 type Props = {
     routes: Array<PageLink>;
@@ -25,7 +27,8 @@ export const HorizontalNav = (props: Props) => {
             const classes = "horizontal-nav__item" + (isCurrentPath ? " horizontal-nav__item--current" : "");
 
             return <a key={route.route} className={classes} onClick={() => goToRoute(route.route)}>
-                {route.label}
+                <Icon iconType={IconType.PLUS} iconTheme={IconTheme.DARK} />
+                <div className="horizontal-nav__item--text">{route.label}</div>
             </a>;
         })}
     </div>;
