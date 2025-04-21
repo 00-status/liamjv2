7bbf41f7ab76811455895eec6e59cb49016a5934
00-status <dedypips@gmail.com>
2025-04-21 09:53:04 -0600

    added tile component and some basic styles

--- src/Kingdom/Overview/KingdomOverviewPage.tsx
+++ src/Kingdom/Overview/KingdomOverviewPage.tsx
@@ -1,55 +1,63 @@
 
 import './kingdom-overview-page.css';
 import { Page } from "../../SharedComponents/Page/Page";
+import { useMemo, useState } from 'react';
+import { Tile } from './Tile';
 
 type Kingdom = { name: String, terrain: Terrain };
 type Terrain = { rowSize: number, columnSize: number, tiles: Array<Tile> };
-type Tile = { x: number, y: number, type: string };
+export type Tile = { x: number, y: number, type: string };
 
 // TODO: Instead of expanding the kingdom's terrain when the kingdom grows
 // we could instead generate a larger 7x7 grid and only display necessary tiles.
 
+const kingdom: Kingdom = {
+    name: "Camelot",
+    terrain: {
+        rowSize: 3,
+        columnSize: 3,
+        tiles: [
+            { x: 1, y: 1, type: "Prairie" },
+            { x: 2, y: 1, type: "Prairie" },
+            { x: 3, y: 1, type: "Prairie" },
+            { x: 1, y: 2, type: "Forest" },
+            { x: 2, y: 2, type: "Forest" },
+            { x: 3, y: 2, type: "Forest" },
+            { x: 1, y: 3, type: "Mountain" },
+            { x: 2, y: 3, type: "Mountain" },
+            { x: 3, y: 3, type: "Mountain" },
+        ]
+    }
+};
+
 const KingdomOverviewPage = () => {
-    const kingdom: Kingdom = {
-        name: "Camelot",
-        terrain: {
-            rowSize: 3,
-            columnSize: 3,
-            tiles: [
-                { x: 1, y: 1, type: "Prairie" },
-                { x: 2, y: 1, type: "Prairie" },
-                { x: 3, y: 1, type: "Prairie" },
-                { x: 1, y: 2, type: "Forest" },
-                { x: 2, y: 2, type: "Forest" },
-                { x: 3, y: 2, type: "Forest" },
-                { x: 1, y: 3, type: "Mountain" },
-                { x: 2, y: 3, type: "Mountain" },
-                { x: 3, y: 3, type: "Mountain" },
-            ]
-        }
-    };
+    const [orderedTiles, setOrderedTiles] = useState<Array<Tile>>([]);
 
-    const tilesByCoords = kingdom.terrain.tiles.reduce((carry, tile) => {
-        const key = `${tile.x}-${tile.y}`;
-        carry[key] = tile;
+    useMemo(() => {
+        const tilesByCoords = kingdom.terrain.tiles.reduce((carry, tile) => {
+            const key = `${tile.x}-${tile.y}`;
+            carry[key] = tile;
+    
+            return carry;
+        }, {} as {[key: string]: Tile} );
 
-        return carry;
-    }, {} as {[key: string]: Tile} );
-
-    const orderedTiles = [];
-    for (let y = 1; y <= kingdom.terrain.columnSize; y++) {
-        for (let x = 1; x <= kingdom.terrain.rowSize; x++) {
-            const tile = tilesByCoords[`${x}-${y}`];
-            if (tile) {
-                orderedTiles.push(tile);
+        const orderedTiles = [];
+        for (let y = 1; y <= kingdom.terrain.columnSize; y++) {
+            for (let x = 1; x <= kingdom.terrain.rowSize; x++) {
+                const tile = tilesByCoords[`${x}-${y}`];
+                if (tile) {
+                    orderedTiles.push(tile);
+                }
             }
         }
-    }
+
+        setOrderedTiles(orderedTiles);
+    }, [kingdom]);
 
     return <Page title="Kingdom" routes={[]}>
         <div className='kingdom-overview-page'>
             <div className='kingdom-overview-page__grid'>
-                {orderedTiles.map(tile => <div key={tile.x + "-" + tile.y}>{tile.type}</div>)}
+                {orderedTiles.map(tile => <Tile key={tile.x + "-" + tile.y} type={tile.type} />)}
             </div>
         </div>
     </Page>;
