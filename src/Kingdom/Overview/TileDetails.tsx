04059068ef8465cdbb4ff38a4bcdb0f181ee97ea
00-status <dedypips@gmail.com>
2026-07-01 11:29:20 -0600

    Updated css so the dropdown doesn't look so terrible in its new card

--- src/Kingdom/Overview/TileDetails.tsx
+++ src/Kingdom/Overview/TileDetails.tsx
@@ -1,5 +1,6 @@
 import { Dispatch, SetStateAction, useState } from 'react';
 
+import './tile-details.css';
 import { Card } from '../../SharedComponents/Card/Card';
 import { Button } from '../../SharedComponents/Button/Button';
 import { Dropdown } from '../../SharedComponents/Dropdown/Dropdown';
@@ -18,51 +19,52 @@ export const TileDetails = (props: Props) => {
     const [buildingName, setBuildingName] = useState<string>('');
     return (
         <Card title={tile.type}>
-            <div>
-                {tile.traits.length > 0 && (
-                    <div>
-                        <h3>Terrain Traits</h3>
-                        <ul>
-                            {tile.traits.map((trait) => (
-                                <li key={trait}>{trait}</li>
-                            ))}
-                        </ul>
-                    </div>
-                )}
-            </div>
-            <div>
-                <div className="tile-details__buildings-form">
-                    <Dropdown
-                        id="modal-building-construct"
-                        label="Construct Buildings"
-                        defaultValue={buildingName}
-                        options={buildingNames}
-                        onOptionSelect={(value) => setBuildingName(value)}
-                        isDisabled={buildings.length > 0}
-                    />
-                    <Button
-                        disabled={!buildingName}
-                        onClick={() => {
-                            setBuildings((state) => [
-                                ...state,
-                                { name: buildingName, assignedTile: tile.id },
-                            ]);
-                            setBuildingName('');
-                        }}
-                    >
-                        Add Building
-                    </Button>
+            <div className="tile-details">
+                <div>
+                    {tile.traits.length > 0 && (
+                        <div>
+                            <h3>Terrain Traits</h3>
+                            <ul>
+                                {tile.traits.map((trait) => (
+                                    <li key={trait}>{trait}</li>
+                                ))}
+                            </ul>
+                        </div>
+                    )}
                 </div>
-                {buildings.length > 0 && (
-                    <div>
-                        <h3>Buildings</h3>
-                        <ul>
-                            {buildings.map((building) => (
-                                <li key={building.name}>{building.name}</li>
-                            ))}
-                        </ul>
+                <div>
+                    <h3>Buildings</h3>
+                    <div className="tile-details__buildings-form">
+                        <Dropdown
+                            id="modal-building-construct"
+                            defaultValue={buildingName}
+                            options={buildingNames}
+                            onOptionSelect={(value) => setBuildingName(value)}
+                            isDisabled={buildings.length > 0}
+                        />
+                        <Button
+                            disabled={!buildingName}
+                            onClick={() => {
+                                setBuildings((state) => [
+                                    ...state,
+                                    { name: buildingName, assignedTile: tile.id },
+                                ]);
+                                setBuildingName('');
+                            }}
+                        >
+                            Add Building
+                        </Button>
                     </div>
-                )}
+                    {buildings.length > 0 && (
+                        <div>
+                            <ul>
+                                {buildings.map((building) => (
+                                    <li key={building.name}>{building.name}</li>
+                                ))}
+                            </ul>
+                        </div>
+                    )}
+                </div>
             </div>
         </Card>
     );
